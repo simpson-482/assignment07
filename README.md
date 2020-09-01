f20894cdc18d7145bb30bdf87cfe74b3296a6b3a
Jared Simpson <simpson.482@osu.edu>
2020-09-01 19:42:08 -0400

    Updated files

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 
-## Assignment07
-README.md generator
+## Project Title 
+Assignment 7-- ReadMe Generator
+## Project Description 
+Using inquier and user inputs this application generates a README file
+## GitHub Username 
 simpson-482
+## Installation Guidelines 
+Because this is a local application download and clone the GitHub link
+## User Instructions 
+Install the NPM on the terminal, then run the script.js command in node
+## Project Contributions 
+Can email me at simpson.482@osu.eud
+## Project License 
+none
+## Project Questions
+none
+
+https://drive.google.com/file/d/1qUWSyOhbG7ciXky-uxJJjqJR7T3XnLS4/view
--- script.js
+++ script.js
@@ -21,7 +21,32 @@ async function start() {
             type: "input",
             message: "What is your Projects description",
             name: "projectDescription"
-        }
+        },
+        {
+            type: "input",
+            message: "Please provide installation instructions here if applicable.",
+            name: "projectInstallation"
+        },
+        {
+            type: "input",
+            message: "Please provide user instructions for the user to run your application here.",
+            name: "projectUserInstructions"
+        },
+        {
+            type: "input",
+            message: "Please add instructions for users to contribute to or report issues with the project here",
+            name: "projectContributions"
+        },
+        {
+            type: "input",
+            message: "Please provide the license type for this project if applicable",
+            name: "projectLicense"
+        },
+        {
+            type: "input",
+            message: "Please list any questions about the project if applicable",
+            name: "projectQuestions"
+        },
         ]);
 
 
@@ -32,11 +57,29 @@ async function start() {
         const myGitUserName = userResponse.username;
         const myProjectTitle = userResponse.projectTitle;
         const myProjectDescription = userResponse.projectDescription;
+        const myProjectInstallation = userResponse.projectInstallation;
+        const myProjectUserInstructions = userResponse.projectUserInstructions;
+        const myProjectContributions = userResponse.projectContributions;
+        const myProjectLicense = userResponse.projectLicense;
+        const myProjectQuestions = userResponse.projectQuestions;
 
         var myResult = (`
-## ${myProjectTitle}
+## Project Title 
+${myProjectTitle}
+## Project Description 
 ${myProjectDescription}
+## GitHub Username 
 ${myGitUserName}
+## Installation Guidelines 
+${myProjectInstallation}
+## User Instructions 
+${myProjectUserInstructions}
+## Project Contributions 
+${myProjectContributions}
+## Project License 
+${myProjectLicense}
+## Project Questions
+${myProjectQuestions}
 `)
 
 var writeResult = fs.writeFileSync(path.join(__dirname, 'README.md'), myResult)
